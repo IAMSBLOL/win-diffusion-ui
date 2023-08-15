63c1ddee49dcd23318d62a74563596fa5eeaf9bd
IAMSBLOL <173537077@qq.com>
2023-08-16 01:22:59 +0800

    init

--- src/renderer/container/app/App.tsx
+++ src/renderer/container/app/App.tsx
@@ -3,9 +3,6 @@ import { Outlet } from 'react-router-dom';
 import './app.module.less'
 // import { useDispatch } from 'react-redux'
 
-// import api from '../api'
-// 可以在这里做登录拦截或者其他
-
 const App = () => {
   return (
     <div styleName='app'>
--- src/renderer/views/HomePage/HomePage.tsx
+++ src/renderer/views/HomePage/HomePage.tsx
@@ -1,10 +1,18 @@
+import { Button } from 'antd'
+// import fs from 'fs'
 import './HomePage.module.less'
+import { useEffect } from 'react'
 
 const HomePage = () => {
+  useEffect(() => {
+    window.electron.ipcRenderer.sendMessage('ipc-example', 123)
+  }, [])
   return (
     <div styleName='HomePage'>
       <div className="test text-cyan-900">
-        HomePage1
+        <Button className='btn btn-primary'>
+          123123
+        </Button>
       </div>
     </div>
   )
