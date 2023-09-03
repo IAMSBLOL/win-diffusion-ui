a0faec5ed89c4bbc994fd04ef2ced7bbe843cd27
IAMSBLOL <173537077@qq.com>
2023-09-04 00:12:41 +0800

    feat：canvas matrxi

--- src/renderer/views/Text2Image/Text2Image.tsx
+++ src/renderer/views/Text2Image/Text2Image.tsx
@@ -4,6 +4,7 @@ import './Text2Image.module.less'
 const HomePage = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const invertSelfMatrix = useRef<DOMMatrix | undefined>()
+  const ctx = useRef<CanvasRenderingContext2D | null>(null)
 
   useEffect(() => {
     window.electron.ipcRenderer.sendMessage('ipc-example', 1231)
@@ -14,21 +15,65 @@ const HomePage = () => {
 
   useEffect(() => {
     if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext('2d')
-      const matrix = ctx?.getTransform()
-      invertSelfMatrix.current = matrix?.invertSelf()
+      const canvas = canvasRef.current
+      canvas.width = canvas.clientWidth * window.devicePixelRatio;
+      canvas.height = canvas.clientHeight * window.devicePixelRatio;
+      ctx.current = canvasRef.current.getContext('2d')
+
+      if (ctx.current) {
+        ctx.current.beginPath()
+        ctx.current.lineWidth = 1
+        ctx.current.strokeStyle = 'red'
+        ctx.current.setTransform(2, 0, 0, 2, 0, 0)
+        // ctx.transform(
+        //   2, 0, 0, 1, 50, 100
+        // )
+        ctx.current.rect(0, 0, 100, 100)
+        ctx.current.stroke()
+        ctx.current.closePath()
+      }
+
+      const matrix = ctx.current?.getTransform()
+
+      console.log(matrix, 'matrix')
+      // invertSelfMatrix.current = matrix?.invertSelf()
+      // console.log(invertSelfMatrix.current)
+      if (matrix) {
+        invertSelfMatrix.current = new DOMMatrix().setMatrixValue(matrix.toString()).invertSelf()
+        console.log(invertSelfMatrix.current, 'invertSelfMatrix')
+      }
     }
   }, [])
 
-  const handleMouseMove = (event: React.MouseEvent) => {
+  const handleClick = (event:React.MouseEvent) => {
     console.log(event, 'event')
+    const { clientX, clientY } = event
+    const viewportPoint = new DOMPoint(clientX, clientY).matrixTransform(invertSelfMatrix.current);
+    console.log(viewportPoint, 'viewportPoint')
+    if (ctx.current) {
+      ctx.current.beginPath()
+      ctx.current.lineWidth = 1
+      ctx.current.strokeStyle = 'red'
+
+      // ctx.transform(
+      //   2, 0, 0, 1, 50, 100
+      // )
+      ctx.current.rect(viewportPoint.x, viewportPoint.y, 100, 100)
+      ctx.current.stroke()
+      ctx.current.closePath()
+      // var canvasRect = canvas.getBoundingClientRect();
+      // 还有 border
+      // var screenX = canvasX * transform.a + canvasY * transform.c + transform.e + canvasRect.left;
+      // var screenY = canvasX * transform.b + canvasY * transform.d + transform.f + canvasRect.top;
+    }
   }
+
   return (
     <div styleName='Text2Image'>
       <canvas
         className='canvas_container'
         ref={canvasRef}
-        onMouseMove={handleMouseMove}
+        onClick={handleClick}
       />
     </div>
   )
